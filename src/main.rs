f301d81b15a472ab7ca03abe866cea2712e53686
mia-deutsch <mia.deutsch@freenet.de>
2025-02-05 14:58:38 +0100

    BusinessLogic usable

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,7 @@
-use thirtyfour::prelude::*;
-use tokio;
-//use BusinessLogic;
+use BusinessLogic::test;
 //use DataUtils;
 #[tokio::main]
-async fn main() -> WebDriverResult<()> {
-    //let selenium_host = String::from("localhost");
-    //let selenium_port = String::from("4444");
-    let selenium_host = std::env::var("SELENIUM_HOST").unwrap();
-    let selenium_port = std::env::var("SELENIUM_PORT").unwrap();
-    let url = format!("http://{}:{}", selenium_host, selenium_port);
-
-    let driver = WebDriver::new(&url, DesiredCapabilities::chrome()).await?;
-
-    driver.get("https://www.rust-lang.org").await?;
-    println!("Page title: {}", driver.title().await?);
-
-    driver.quit().await?;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    test().await?;
     Ok(())
 }
